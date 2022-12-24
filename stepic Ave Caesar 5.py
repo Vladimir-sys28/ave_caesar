906b903489b212e84067904b8ca63f56e947993d
Vladimir-sys28 <75503221+Vladimir-sys28@users.noreply.github.com>
2022-12-24 23:13:16 +0300

    Update stepic Ave Caesar 5.py
    При вводе предложения, которое оканчивается на букву, скрипт добавляет к предложению "!" и работает по ветке if.
    Если предложение оканчивается на ".", "!" или "?" скрипт работает по ветке else:

--- stepic Ave Caesar 5.py
+++ stepic Ave Caesar 5.py
@@ -1,45 +1,70 @@
 text = input()
+if text[-1].isalpha():
+    text += '!'
 
 
-def sdvig():
-    count = 0
-    s = []
-    for i in range(len(text)):
-        if text[i].isalpha():
-            count += 1
-        elif not text[i].isalpha():
-            s.append(count)
-            count = 0
-    s = [x for x in s if x != 0]
-    return s
+    def sdvig():
+        count = 0
+        s = []
+        for i in range(len(text)):
+            if text[i].isalpha():
+                count += 1
+            elif not text[i].isalpha():
+                s.append(count)
+                count = 0
+        s = [x for x in s if x != 0]
+        return s
 
 
-sdvig()
-z = sdvig()
-# print(z)
-# print(len(z))
+    z = sdvig()
+    for y in range(len(z)):
+        alf_low = ['абвгдежзийклмнопрстуфхцчшщъыьэюя' * 2, 'abcdefghijklmnopqrstuvwxyz' * 2]
+        alf_upp = ['АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' * 2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 2]
+        language = 1
+        action = 0
+        key = z[y] % [32, 26][language]
+        result_text = ''
+        for c in text[0:-1]:
+            if c.isalpha() and c.islower():
+                result_text += alf_low[language][
+                    alf_low[language].find(c) + len(alf_low[language]) // 2 * action + [1, -1][action] * key]
+            elif c.isalpha() and c.isupper():
+                result_text += alf_upp[language][
+                    alf_upp[language].find(c) + len(alf_low[language]) // 2 * action + [1, -1][action] * key]
+            else:
+                result_text += c
+        out_lis_text = result_text.split()
+        print(out_lis_text[y], end=' ')
+else:
+    def sdvig():
+        count = 0
+        s = []
+        for i in range(len(text)):
+            if text[i].isalpha():
+                count += 1
+            elif not text[i].isalpha():
+                s.append(count)
+                count = 0
+        s = [x for x in s if x != 0]
+        return s
 
 
-# y = 0
-for y in range(len(z)):
-    # print(text[y])
-    alf_low = ['абвгдежзийклмнопрстуфхцчшщъыьэюя' * 2, 'abcdefghijklmnopqrstuvwxyz' * 2]
-    alf_upp = ['АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' * 2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 2]
-    language = 1
-    action = 0
-# text = input('Введите исходный текст: ')
-    key = z[y] % [32, 26][language]
-    result_text = ''
-    for c in text:
-        if c.isalpha() and c.islower():
-            result_text += alf_low[language][
-                alf_low[language].find(c) + len(alf_low[language]) // 2 * action + [1, -1][action] * key]
-        elif c.isalpha() and c.isupper():
-            result_text += alf_upp[language][
-                alf_upp[language].find(c) + len(alf_low[language]) // 2 * action + [1, -1][action] * key]
-        else:
-            result_text += c
-
-    print(result_text)
-for u in range(len(z)):
-    print(u, end=' ')
+    z = sdvig()
+    for y in range(len(z)):
+        alf_low = ['абвгдежзийклмнопрстуфхцчшщъыьэюя' * 2, 'abcdefghijklmnopqrstuvwxyz' * 2]
+        alf_upp = ['АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' * 2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 2]
+        language = 1
+        action = 0
+        key = z[y] % [32, 26][language]
+        result_text = ''
+        for c in text:
+            if c.isalpha() and c.islower():
+                result_text += alf_low[language][
+                    alf_low[language].find(c) + len(alf_low[language]) // 2 * action + [1, -1][action] * key]
+            elif c.isalpha() and c.isupper():
+                result_text += alf_upp[language][
+                    alf_upp[language].find(c) + len(alf_low[language]) // 2 * action + [1, -1][action] * key]
+            else:
+                result_text += c
+        out_lis_text = result_text.split()
+        print(out_lis_text[y], end=' ')
